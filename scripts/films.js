bd881ffad708bad9d8f71c3576c61cbeec50d14c
joshin-run <jahmsc@gmail.com>
2017-05-29 18:28:55 -0600

    Fix (main) Delete undefined 'str', other small fix

--- scripts/films.js
+++ scripts/films.js
@@ -146,80 +146,8 @@ window.onload = function starWarsFilms () {
       function epsSevenDate() {
       document.getElementById("p-output-box-seven").innerHTML = ep7Date;
     }
-
-
-      //jumbled up text below
-
-      // str1 = JSON.stringify(results[0].opening_crawl);
-      // console.log('results[0].opening_crawl) ', results[0].opening_crawl)
-      // document.getElementById('p-output-box-one').innerHTML = str1;
-      // console.log('string 1', str1)
-
-    }
-    else {
-      console.log('no data was returned')
-    }
-    document.getElementById('p-output-box-zero').innerHTML = str;
-
-
+  } else {
+    console.log('no data was returned')
+  }
   })
 }
-
-// var lastIndex = "";
-//   function listQ(){
-//   var e = document.getElementById("list");
-//   if(e.selectedIndex > 0){
-//     if(e.selectedIndex != lastIndex) {
-//       if(1 === e.options[e.selectedIndex].value)
-//       episode1 = results[0].opening_crawl;
-//       document.getElementById('p-output-box-one').innerHTML = episode1;
-//       lastIndex = e.selectedIndex;
-//       if(2 === e.options[e.selectedIndex].value)
-//       episodeDate = results[0].release_date;
-//       document.getElementById('p-output-box-one').innerHTML = episodeDate;
-//       lastIndex = e.selectedIndex;
-//       alert("realease date")
-//     }
-//     else {
-//       lastIndex = ""
-//     }
-//   }
-// }
-// document.getElementById("list").addEventListener("click",listQ);
-
-// var lastIndex = "";
-//   function listQ(){
-//   var e = document.getElementById("list");
-//   if(e.selectedIndex > 0){
-//     if(e.selectedIndex != lastIndex) {
-//       if(1 === e.options[e.selectedIndex].value)
-//         document.getElementById("p-output-box-one").inner
-//       lastIndex = e.selectedIndex;
-//       if(2 === e.options[e.selectedIndex].value)
-//         alert("yo");
-//       lastIndex = e.selectedIndex;
-//     }
-//     else {
-//       lastIndex = ""
-//     }
-//   }
-// }
-// document.getElementById("list").addEventListener("click",listQ);
-
-// <select id="list">
-//   <option value="1">One</option>
-//   <option value="2">Two</option>
-//   <option value="3">Three</option>
-//   <option value="Blank Test">Four</option>
-// </select>
-//
-// <script type="text/javascript">
-//   function swList() {
-//     let input = document.getElementById("sw-list");
-//     if(input.selectedIndex > 0){
-//       if("Blank Test" === input.options[input.selectedIndex].value){ alert("yo"); }
-//     }
-//   }
-//   document.getElementById("sw-list").addEventListener("click", swList);
-//   document.getElementByID("p-output-box-one").innerHTML
-// </script>
